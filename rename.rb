a0358d5ecc95eebef17acd09e4b8fea838d97345
stunt <stunt@hac.ks>
2025-10-21 23:47:11 +0200

    Add enemy assets

---
+++ rename.rb
@@ -0,0 +1,28 @@
+Dir.entries(".").each do |filename|
+  next unless match = filename.match(/^enemy(?<color>[A-Za-z]+)(?<number>\d+)\.png$/)
+  color  = match[:color]
+  number = match[:number].to_i
+
+  classes = {
+    "Red"     => "Armor",
+    "Blue"    => "Shield",
+    "Green"   => "Mixed",
+    "Black"   => "Hull",
+    "DeepRed" => "Phasing",
+    "Purple"  => "MixedPhasing"
+  }
+
+  types = {
+    1 => "Fighter",
+    2 => "Skirmisher",
+    3 => "Sniper",
+    4 => "Juggernaut",
+    5 => "Frigate",
+    7 => "Gatling",
+    8 => "Missiles",
+    10 => "Bulwark",
+  }
+
+  next if (types[number].nil? || classes[color].nil?)
+  File.rename filename, "Enemy_#{types[number]}#{classes[color]}.png"
+end
