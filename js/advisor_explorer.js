088888aa33d20cc0f65e0ecbc8648e6e15843202
stunt <stunt@hac.ks>
2025-11-12 23:01:48 +0100

    More advisor explorer stuff

--- js/advisor_explorer.js
+++ js/advisor_explorer.js
@@ -1,3 +1,42 @@
+const galaxies = {
+    g1: {
+        maxStats: 2.75,
+        minStats: 1.0,
+        battles: [
+            {
+                name: "Battle 1",
+                id: "1_b1",
+                color: "red",
+            },
+            {
+                name: "Battle 2",
+                id: "2_b2",
+                color: "red",
+            },
+            {
+                name: "Battle 3",
+                id: "3_b3",
+                color: "red",
+            },
+            {
+                name: "Battle 4",
+                id: "4_b4",
+                color: "red",
+            },
+            {
+                name: "Battle 5",
+                id: "4_b5",
+                color: "red",
+            },
+            {
+                name: "Battle 6",
+                id: "6_b6",
+                color: "white",
+            }
+        ]
+    }
+}
+
 function initAdvisorExplorer() {
-    alert("It works!");
+    
 }
