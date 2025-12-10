fbf065bf791c0717d5faeea1a124a8276e644f3d
stunt <stunt@hac.ks>
2025-12-10 02:10:21 +0100

    Fix synth tier color, fix viewport

--- usi.js
+++ usi.js
@@ -355,7 +355,7 @@ function init() {
 
   // set viewport
   const viewport = document.querySelector("meta[name=viewport]");
-  viewport.setAttribute("content", "width=device-width, initial-scale=1.0, minimum-scale=1.0");
+  //viewport.setAttribute("content", "width=device-width, initial-scale=1.0, minimum-scale=1.0");
 
 
   synthFix();
